95cd1953a49ff45e62af18d6a095f58713d7e2f5
107061110 <chebsb229@gmail.com>
2020-05-13 18:16:24 +0800

    ok

--- src/model_deploy/main.cpp
+++ src/model_deploy/main.cpp
@@ -13,42 +13,37 @@
 #include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/version.h"
 #define songlength 20
-#define numOfSong 3
-#define CircuIncre(a) ((a + 1 >= numOfSong)? 0: a+1)
+#define SongNum 3
+#define PCSongNum 6
+#define CircuIncre(a, N) ((a + 1 >= N)? 0: a+1)
 #define CircuDecre(a) ((a - 1 < 0)? 2: a-1)
 
-int PredictGesture(float* output) {
+int PredictGesture(float* output) 
+{
 	// How many times the most recent gesture has been matched in a row
 	static int continuous_count = 0;
 	// The result of the last prediction
 	static int last_predict = -1;
-
 	// Find whichever output has a probability > 0.8 (they sum to 1)
 	int this_predict = -1;
-	for (int i = 0; i < label_num; i++) {
-		if (output[i] > 0.8) this_predict = i;
-	}
 
+	for (int i = 0; i < label_num; i++) 
+		if (output[i] > 0.8) this_predict = i;
 	// No gesture was detected above the threshold
 	if (this_predict == -1) {
 		continuous_count = 0;
 		last_predict = label_num;
 		return label_num;
 	}
-
-	if (last_predict == this_predict) {
+	if (last_predict == this_predict) 
 		continuous_count += 1;
-	}
-	else {
+	else 
 		continuous_count = 0;
-	}
 	last_predict = this_predict;
-
 	// If we haven't yet had enough consecutive matches for this gesture,
 	// report a negative result
-	if (continuous_count < config.consecutiveInferenceThresholds[this_predict]) {
+	if (continuous_count < config.consecutiveInferenceThresholds[this_predict]) 
 		return label_num;
-	}
 	// Otherwise, we've seen a positive result, so clear all our variables
 	// and report it
 	continuous_count = 0;
@@ -71,9 +66,9 @@ EventQueue queue(32 * EVENTS_EVENT_SIZE);
 EventQueue queue1(32 * EVENTS_EVENT_SIZE);
 EventQueue queue2(32 * EVENTS_EVENT_SIZE);
 EventQueue queueM(32 * EVENTS_EVENT_SIZE);
-Note song[numOfSong][songlength];	// 3 songs to choose
+Note song[SongNum][songlength];	// 3 songs to choose
 int16_t waveform[kAudioTxBufferSize];
-int16_t stopNote[kAudioTxBufferSize] = {0};
+int16_t stopNote[kAudioTxBufferSize] = { 0 };
 
 Thread t;
 Thread t1;
@@ -81,13 +76,15 @@ Thread t2;
 Thread tM;
 int state = 0;
 int songI = 0;	// song index
-int noteI = -1;  // note index
+int noteI = -1; // note index
+int songIn = 3;	// song to call from pc
 void sw2_rise();
 void sw3_rise();
 int idc;
 int idb;
 void gestureModeSelect();
 void gestureSongSelect();
+void gestureSongSwitch();
 void playNote(int);
 void playSong(int, int);
 void PlayMode();
@@ -98,20 +95,131 @@ bool should_clear_buffer = false;
 bool got_data = false;
 int gesture_index;
 
-int main(int argc, char* argv[]) {
-	char list[numOfSong][songlength * 2 + 10];
+void songDecode()
+{
+	int &j = songI;
+	char st[songlength * 2 + 10];
+	pc.printf("%d\r\n", songIn);
+	uLCD.cls();
+	uLCD.text_width(2);
+	uLCD.text_height(4);
+	uLCD.printf("\nAwaiting\nPC Input\n");
+	while (pc.readable() == false);
+	if (pc.readable()) {
+		pc.scanf("%s", st);
+		wait(0.8);
+	}
+	for (int i = 0, k = 0; k < 2 * songlength; k++) {
+		switch (st[k])
+		{
+		case 'w':
+			song[j][i++].len = 1;
+			break;
+		case 'x':
+			song[j][i++].len = 2;
+			break;
+		case 'y':
+			song[j][i++].len = 3;
+			break;
+		case 'z':
+			song[j][i++].len = 4;
+			break;
+		case 'c':
+			song[j][i].f = 261;
+			break;
+		case 'd':
+			song[j][i].f = 294;
+			break;
+		case 'e':
+			song[j][i].f = 330;
+			break;
+		case 'f':
+			song[j][i].f = 349;
+			break;
+		case 'g':
+			song[j][i].f = 392;
+			break;
+		case 'a':
+			song[j][i].f = 440;
+			break;
+		case 'j':
+			song[j][i].f = 277;
+			break;
+		case 'k':
+			song[j][i].f = 311;
+			break;
+		case 'l':
+			song[j][i].f = 370;
+			break;
+		case 'm':
+			song[j][i].f = 415;
+			break;
+		case 'n':
+			song[j][i].f = 466;
+			break;
+		case 'b':
+			song[j][i].f = 494;
+			break;
+		case 'C':
+			song[j][i].f = 523;
+			break;
+		case 'D':
+			song[j][i].f = 587;
+			break;
+		case 'E':
+			song[j][i].f = 659;
+			break;
+		case 'F':
+			song[j][i].f = 698;
+			break;
+		case 'G':
+			song[j][i].f = 784;
+			break;
+		case 'A':
+			song[j][i].f = 880;
+			break;
+		case 'B':
+			song[j][i].f = 988;
+			break;
+		case 'J':
+			song[j][i].f = 554;
+			break;
+		case 'K':
+			song[j][i].f = 622;
+			break;
+		case 'L':
+			song[j][i].f = 740;
+			break;
+		case 'M':
+			song[j][i].f = 831;
+			break;
+		case 'N':
+			song[j][i].f = 932;
+			break;
+		case 's':
+			song[j][i].f = 0;
+		}
+	}
+	return;
+}
+
+int main(int argc, char* argv[]) 
+{
+	char list[SongNum][songlength * 2 + 10];
 	deboun1.start();
 	deboun2.start();
 	uLCD.text_width(2);
 	uLCD.text_height(4);
 	uLCD.printf("\nAwaiting\nPC input\n");
+	pc.printf("9\r\n");
+	while (pc.readable() == false);
 	// load 3 songs	
-	for (int j = 0; j < numOfSong;)
+	for (int j = 0; j < SongNum;)
 		if (pc.readable()) {
 			pc.scanf("%s", list[j++]);
 			wait(0.8);
 		}
-	for (int j = 0; j < numOfSong; j++)
+	for (int j = 0; j < SongNum; j++)
 		for (int i = 0, k = 0; k < 2 * songlength; k++) {
 			switch (list[j][k])
 			{
@@ -145,6 +253,21 @@ int main(int argc, char* argv[]) {
 			case 'a':
 				song[j][i].f = 440;
 				break;
+			case 'j':
+				song[j][i].f = 277;
+				break;
+			case 'k':
+				song[j][i].f = 311;
+				break;
+			case 'l':
+				song[j][i].f = 370;
+				break;
+			case 'm':
+				song[j][i].f = 415;
+				break;
+			case 'n':
+				song[j][i].f = 466;
+				break;
 			case 'b':
 				song[j][i].f = 494;
 				break;
@@ -168,6 +291,21 @@ int main(int argc, char* argv[]) {
 				break;
 			case 'B':
 				song[j][i].f = 988;
+				break;
+			case 'J':
+				song[j][i].f = 554;
+				break;
+			case 'K':
+				song[j][i].f = 622;
+				break;
+			case 'L':
+				song[j][i].f = 740;
+				break;
+			case 'M':
+				song[j][i].f = 831;
+				break;
+			case 'N':
+				song[j][i].f = 932;
 				break;
 			case 's':
 				song[j][i].f = 0;
@@ -213,10 +351,12 @@ void gestureModeSelect()
 	int input_length = model_input->bytes / sizeof(float);
 	TfLiteStatus setup_status = SetupAccelerometer(error_reporter);
 	if (setup_status != kTfLiteOk) {
-		error_reporter->Report("Set up failed\n");
+		uLCD.cls();
+		uLCD.text_width(2);
+		uLCD.text_height(3);
+		uLCD.printf("\nexceptio\ntake place\n");
 		return;
 	}
-	error_reporter->Report("Set up successful...\n");
 	uLCD.cls();
 	uLCD.text_width(2);
 	uLCD.text_height(3);
@@ -239,7 +379,10 @@ void gestureModeSelect()
 		// Run inference, and report any error
 		TfLiteStatus invoke_status = interpreter->Invoke();
 		if (invoke_status != kTfLiteOk) {
-			error_reporter->Report("Invoke failed on index: %d\n", begin_index);
+			uLCD.cls();
+			uLCD.text_width(2);
+			uLCD.text_height(3);
+			uLCD.printf("\nInvoke\nfailed on\nindex: %d\n", begin_index);
 			continue;
 		}
 		// Analyze the results to obtain a prediction
@@ -255,7 +398,7 @@ void gestureModeSelect()
 				state = 4;
 				uLCD.cls();
 				uLCD.text_width(2);
-				uLCD.text_height(3);
+				uLCD.text_height(2);
 				uLCD.printf("\nGo to\nsong\nselect?\n");
 				return;
 			case 1:	// slope
@@ -307,10 +450,12 @@ void gestureSongSelect()
 	int input_length = model_input->bytes / sizeof(float);
 	TfLiteStatus setup_status = SetupAccelerometer(error_reporter);
 	if (setup_status != kTfLiteOk) {
-		error_reporter->Report("Set up failed\n");
+		uLCD.cls();
+		uLCD.text_width(2);
+		uLCD.text_height(3);
+		uLCD.printf("\nexceptio\ntake place\n");
 		return;
 	}
-	error_reporter->Report("Set up successful...\n");
 	uLCD.cls();
 	uLCD.text_width(2);
 	uLCD.text_height(3);
@@ -333,7 +478,10 @@ void gestureSongSelect()
 		// Run inference, and report any error
 		TfLiteStatus invoke_status = interpreter->Invoke();
 		if (invoke_status != kTfLiteOk) {
-			error_reporter->Report("Invoke failed on index: %d\n", begin_index);
+			uLCD.cls();
+			uLCD.text_width(2);
+			uLCD.text_height(3);
+			uLCD.printf("\nInvoke\nfailed on\nindex: %d\n", begin_index);
 			continue;
 		}
 		// Analyze the results to obtain a prediction
@@ -343,7 +491,6 @@ void gestureSongSelect()
 
 		// Produce an output
 		if (gesture_index < label_num && state == 5) {
-			error_reporter->Report(config.output_message[gesture_index]);
 			switch (gesture_index)
 			{
 			case 0:	// ring
@@ -354,19 +501,120 @@ void gestureSongSelect()
 				uLCD.printf("\nGo to\nSong %d?\n", songI);
 				return;
 			case 1:	// slope
-				songI = CircuDecre(songI);
-				uLCD.cls();
-				uLCD.text_width(2);
-				uLCD.text_height(2);
-				uLCD.printf("\n<- <-\nNow at:\nSong.%d\n", songI);
-				ThisThread::sleep_for(360);
-				continue;
-			case 2:	// sprint
-				songI = CircuIncre(songI);
+				songI = CircuIncre(songI, SongNum);
 				uLCD.cls();
 				uLCD.text_width(2);
 				uLCD.text_height(2);
 				uLCD.printf("\n-> ->\nNow at:\nSong.%d\n", songI);
+				ThisThread::sleep_for(360);
+				continue;
+			case 2:	// sprint
+				songI = CircuIncre(songI, SongNum);
+				uLCD.cls();
+				uLCD.text_width(2);
+				uLCD.text_height(2);
+				uLCD.printf("\n-> ->\nNow at:\nSong.%d\n", songI);
+				ThisThread::sleep_for(360);
+				continue;
+			}
+		}
+	}
+}
+void gestureSongSwitch()	// call new song from pc
+{
+	static tflite::MicroErrorReporter micro_error_reporter;
+	tflite::ErrorReporter* error_reporter = &micro_error_reporter;
+	const tflite::Model* model = tflite::GetModel(g_magic_wand_model_data);
+
+	static tflite::MicroOpResolver<6> micro_op_resolver;
+	micro_op_resolver.AddBuiltin(
+		tflite::BuiltinOperator_DEPTHWISE_CONV_2D,
+		tflite::ops::micro::Register_DEPTHWISE_CONV_2D());
+	micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_MAX_POOL_2D,
+		tflite::ops::micro::Register_MAX_POOL_2D());
+	micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_CONV_2D,
+		tflite::ops::micro::Register_CONV_2D());
+	micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_FULLY_CONNECTED,
+		tflite::ops::micro::Register_FULLY_CONNECTED());
+	micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_SOFTMAX,
+		tflite::ops::micro::Register_SOFTMAX());
+	micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_RESHAPE,
+		tflite::ops::micro::Register_RESHAPE(), 1);
+
+
+	static tflite::MicroInterpreter static_interpreter(
+		model, micro_op_resolver, tensor_arena, kTensorArenaSize, error_reporter);
+	tflite::MicroInterpreter* interpreter = &static_interpreter;
+	interpreter->AllocateTensors();
+	TfLiteTensor* model_input = interpreter->input(0);
+
+	int input_length = model_input->bytes / sizeof(float);
+	TfLiteStatus setup_status = SetupAccelerometer(error_reporter);
+	if (setup_status != kTfLiteOk) {
+		uLCD.cls();
+		uLCD.text_width(2);
+		uLCD.text_height(3);
+		uLCD.printf("\nexceptio\ntake place\n");
+		return;
+	}
+	uLCD.cls();
+	uLCD.text_width(2);
+	uLCD.text_height(3);
+	uLCD.printf("\nPC\nSong\nSelect\n");
+	wait(0.2);
+	while (state == 7) {
+		// Attempt to read new data from the accelerometer
+		got_data = ReadAccelerometer(error_reporter, model_input->data.f,
+			input_length, should_clear_buffer);
+		// If there was no new data,
+		// don't try to clear the buffer again and wait until next time
+		if (!got_data) {
+			should_clear_buffer = false;
+			continue;
+		}
+		uLCD.cls();
+		uLCD.text_width(2);
+		uLCD.text_height(3);
+		uLCD.printf("\n...\n");
+		// Run inference, and report any error
+		TfLiteStatus invoke_status = interpreter->Invoke();
+		if (invoke_status != kTfLiteOk) {
+			uLCD.cls();
+			uLCD.text_width(2);
+			uLCD.text_height(3);
+			uLCD.printf("\nInvoke\nfailed on\nindex: %d\n", begin_index);
+			continue;
+		}
+		// Analyze the results to obtain a prediction
+		gesture_index = PredictGesture(interpreter->output(0)->data.f);
+		// Clear the buffer next time we read data
+		should_clear_buffer = gesture_index < label_num;
+
+		// Produce an output
+		if (gesture_index < label_num && state == 7) {
+			switch (gesture_index)
+			{
+			case 0:	// ring
+				state = 8;
+				uLCD.cls();
+				uLCD.text_width(2);
+				uLCD.text_height(3);
+				uLCD.printf("\nImport\nSong %d?\n", songIn);
+				return;
+			case 1:	// slope
+				songIn = CircuIncre(songIn, PCSongNum);
+				uLCD.cls();
+				uLCD.text_width(2);
+				uLCD.text_height(2);
+				uLCD.printf("\n-> ->\nNow at:\nSong.%d\n", songIn);
+				ThisThread::sleep_for(360);
+				continue;
+			case 2:	// sprint
+				songIn = CircuIncre(songIn, PCSongNum);
+				uLCD.cls();
+				uLCD.text_width(2);
+				uLCD.text_height(2);
+				uLCD.printf("\n-> ->\nNow at:\nSong.%d\n", songIn);
 				ThisThread::sleep_for(360);
 				continue;
 			}
@@ -378,9 +626,9 @@ void playNote(int freq)
 {
 	if (freq != 0) {
 		for (int i = 0; i < kAudioTxBufferSize; i++)
-		{	
-			waveform[i] = (int16_t)(sin((double)i * 2. * M_PI 
-			/ (double)(kAudioSampleFrequency / freq)) * ((1 << 16) - 1));
+		{
+			waveform[i] = (int16_t)(sin((double)i * 2. * M_PI
+				/ (double)(kAudioSampleFrequency / freq)) * ((1 << 16) - 1));
 		}
 		audio.spk.play(waveform, kAudioTxBufferSize);
 	}
@@ -400,7 +648,7 @@ void playSong(int j, int sp = 0)
 }
 void PlayMode()
 {
-	for (int j = 0; state == 0 && j < numOfSong;)
+	for (int j = 0; state == 0 && j < SongNum;)
 	{
 		if (state == 0) {
 			uLCD.cls();
@@ -412,10 +660,10 @@ void PlayMode()
 			playSong(j, noteI + 1);
 			// so that pause won't change songI
 			if (state == 0) {
-				j = CircuIncre(j);
+				j = CircuIncre(j, SongNum);
 				songI = j;
 			}
-			else 
+			else
 				audio.spk.play(stopNote, kAudioTxBufferSize);
 		}
 	}
@@ -438,7 +686,7 @@ void sw2_rise()
 			queueM.call(PlayMode);
 			break;
 		case 3:	// confim next
-			songI = CircuIncre(songI);
+			songI = CircuIncre(songI, SongNum);
 			noteI = -1;
 			state = 0;
 			wait(0.1);
@@ -453,6 +701,12 @@ void sw2_rise()
 			state = 0;
 			wait(0.1);
 			queueM.call(PlayMode);
+			break;
+		case 8:
+			state = 0;
+			noteI = -1;
+			songDecode();
+			queueM.call(PlayMode);
 		}
 		deboun1.reset();
 	}
@@ -461,6 +715,11 @@ void sw3_rise()
 {
 	if (deboun2.read_ms() > 500) {
 		switch (state) {
+		case 0:
+			state = 7;
+			wait(0.1);
+			idb = queue1.call(gestureSongSwitch);
+			break;
 		case 1: // back to playMode
 			queue1.cancel(idc);
 			state = 0;
@@ -482,6 +741,16 @@ void sw3_rise()
 		case 6:	// confim song selecte
 			state = 5;
 			idb = queue1.call(gestureSongSelect);
+			break;
+		case 7:	// back to playMode
+			queue1.cancel(idb);
+			state = 0;
+			wait(0.1);
+			queueM.call(PlayMode);
+			break;
+		case 8:
+			state = 7;
+			idb = queue1.call(gestureSongSwitch);
 		}
 		deboun2.reset();
 	}
--- src/model_deploy/songList.py
+++ src/model_deploy/songList.py
@@ -1,25 +1,52 @@
-import numpy as np
 import serial
 import time
 
-waitTime = 1.0
-
 # generate the song List
-signal1 = "cxcxgxgxaxaxgyswfxfxexexdxdxcyswswswswsw\n"
-signal2 = "gxexeyswfxdxdyswcxdxexfxgxgxgyswswswswsw\n"
-signal3 = "ewfwgwcxcwcwdwewfxawCwawfwexgwfwdwgwcxsw\n"
+song0 = "cxcxgxgxaxaxgyswfxfxexexdxdxcyswswswswsw\n"
+song1 = "gxexeyswfxdxdyswcxdxexfxgxgxgyswswswswsw\n"
+song2 = "ewfwgwcxcwcwdwewfxawCwawfwexgwfwdwgwcxsw\n"
+song3 = "axaxbyswaxaxbyswaxbxCxDwEwDxaxDyswaxexsw\n"
+song4 = "awbwCybwCxExbzsxexaygwaxCxgzsxexfxCwbysw\n"
+song5 = "LwMwAwAwBwAwMxJwEwLwLwEwDwEyswEwDwDwCwsw\n"
+Songlist = [song0, song1, song2, song3, song4, song5]
 
-# send the waveform table to K66F
 serdev = '/dev/ttyACM0'
 s = serial.Serial(serdev)
-print("Sending signal ...")
-print("It may take about %d seconds ..." % (int(3 * waitTime)))
-
-s.write(signal1.encode())
-time.sleep(waitTime)
-s.write(signal2.encode())
-time.sleep(waitTime)
-s.write(signal3.encode())
-time.sleep(waitTime)
-s.close()
-print("Signal sended")
+ 
+line = s.readline() # Read 
+print("Here PC receive:", line)
+sA = line.split()
+i = len(sA) - 2
+if i >= 0:
+    k = int(sA[i])
+    print("k:", k)
+    time.sleep(1.0)
+    if k <= 5 and k >= 0:
+        print("Sending new song: Song ", k)
+        print("It may take about 1 second ..." )
+        s.write(Songlist[k].encode())
+        time.sleep(1.0)
+        s.close()
+        print("New Song arrived!")
+    else:
+        print("Sending basic songlist ...")
+        print("It may take about 3 seconds ...")
+        s.write(Songlist[0].encode())
+        time.sleep(1.0)
+        s.write(Songlist[1].encode())
+        time.sleep(1.0)
+        s.write(Songlist[2].encode())
+        time.sleep(1.0)
+        s.close()
+        print("Signal sended")
+else:
+    print("Sending basic songlist ...")
+    print("It may take about 3 seconds ...")
+    s.write(Songlist[0].encode())
+    time.sleep(1.0)
+    s.write(Songlist[1].encode())
+    time.sleep(1.0)
+    s.write(Songlist[2].encode())
+    time.sleep(1.0)
+    s.close()
+    print("Signal sended")
